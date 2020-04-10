a3746c0bd5b7a8b8a9bf186643de1c7032b85a77
maui <maui@devf.mx>
2020-04-09 21:40:03 -0500

    Agregar modificaciones para reparar rama master

--- database/mongoClient.js
+++ database/mongoClient.js
@@ -1,7 +1,7 @@
 // Database
 const mongoose = require('mongoose');
 
-const DB_URI = "mongodb+srv://maui:abc123def@testing-cluster-efwi5.mongodb.net/supermercado?retryWrites=true";
+const DB_URI = "mongodb+srv://admin:abc123def@testing-cluster-efwi5.mongodb.net/supermercado?retryWrites=true";
 
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then( () => console.log("-Succesful connection to database-"))
