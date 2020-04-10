0dfea8355a46edfef4aa5ae431943d6622d79a2f
maui <maui@devf.mx>
2020-04-09 21:46:59 -0500

    Agregar archivo html como respuesta de un endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('./database/mongoClient');
 const express = require('express');
 const app = express();
+const path = require('path'); 
 const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
@@ -8,6 +9,11 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
+});
+
+// viewed at http://localhost:8080
+app.get('/docs', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use('/api/v1', require('./router'));
