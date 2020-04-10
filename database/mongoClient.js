29fbcfde5d231ffc6a9d91ac549bd7b8ab4c5d40
maui <maui@devf.mx>
2020-04-09 21:52:26 -0500

    Agregar manejo de variables de entorno con dotenv

--- database/mongoClient.js
+++ database/mongoClient.js
@@ -1,8 +1,6 @@
 // Database
 const mongoose = require('mongoose');
 
-const DB_URI = "mongodb+srv://admin:abc123def@testing-cluster-efwi5.mongodb.net/supermercado?retryWrites=true";
-
-mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then( () => console.log("-Succesful connection to database-"))
         .catch( err => console.log(err) );
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 require('./database/mongoClient');
 const express = require('express');
 const app = express();
