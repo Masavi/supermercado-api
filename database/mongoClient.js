53f26d2c59d4c3b93d85413b5b60cca788d4a5e6
maui <maui@devf.mx>
2020-04-09 19:56:02 -0500

    agregar configuracion de conexion a base de datos

--- database/mongoClient.js
+++ database/mongoClient.js
@@ -1,8 +1,8 @@
 // Database
 const mongoose = require('mongoose');
 
-const DB_URI = "mongodb+srv://maui:abc123def@testing-cluster-efwi5.mongodb.net/test?retryWrites=true";
+const DB_URI = "mongodb+srv://admin:abc123def@testing-cluster-efwi5.mongodb.net/supermercado?retryWrites=true";
 
-mongoose.connect(DB_URI, { useNewUrlParser: true })
-        .then( () => console.log(" 🐌-Succesful connection to database-🐌 "))
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then( () => console.log("-Succesful connection to database-"))
         .catch( err => console.log(err) );
