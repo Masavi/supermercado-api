92eacfb80e4e2955fdd97484aaf75fd9a04f7df4
maui <maui@devf.mx>
2020-04-13 18:17:29 -0500

    Agregar paquete para permitir CORS

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 require('./database/mongoClient');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const path = require('path'); 
 const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
